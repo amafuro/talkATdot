85b169dea37b119de76b57b16b0eb940bfbc128e
amafuro <yanagisawa.naoki.24@gmail.com>
2022-09-11 17:05:43 +0900

    initial commit

--- talkATdot_manage/settings.py
+++ talkATdot_manage/settings.py
@@ -98,15 +98,15 @@ WSGI_APPLICATION = 'talkATdot_manage.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 """
 herokuだとsqlite3が動かない
-herokuで使えるpostgresql_psycopg2にする
+herokuで使えるpostgresql_psycopg2にする"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-"""
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -117,7 +117,7 @@ DATABASES = {
         'PORT': '',
     }
 }
-
+"""
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 

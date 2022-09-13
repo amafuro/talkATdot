edee55d4fcfca6eb6c8598b5b5cd28ae6458ef8f
amafuro <yanagisawa.naoki.24@gmail.com>
2022-09-13 14:44:53 +0900

    design commit

--- talkATdot_manage/local_settings.py
+++ talkATdot_manage/local_settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 #settings.pyからそのままコピー
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = ''
+SECRET_KEY = 'django-insecure-&+fd6gh(m^@t*1*%+0x_0=^qb)_7+s5b$kub&-10)l&$nkc7d7'
 
 #settings.pyからそのままコピー
 DATABASES = {
